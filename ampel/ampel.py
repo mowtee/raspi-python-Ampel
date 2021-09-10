c2bf51c62c78e81e11260a4ed2ee8133dcfb1986
mowtee <themowtee@protonmail.com>
2021-09-10 13:40:44 +0200

    added try/except block to ensure gpio.cleanup()

--- ampel/ampel.py
+++ ampel/ampel.py
@@ -7,18 +7,26 @@ gpio.setup(29,gpio.OUT)         # rot
 gpio.setup(31,gpio.OUT)         # gelb
 gpio.setup(33,gpio.OUT)         # gruen
 
-for i in range(3):              # Ampel 3 mal durchschalten
-  gpio.output(29,gpio.HIGH)
-  time.sleep(3)
-  gpio.output(31,gpio.HIGH)
-  time.sleep(1)
-  gpio.output(29,gpio.LOW)
-  gpio.output(31,gpio.LOW)
-  gpio.output(33,gpio.HIGH)
-  time.sleep(3)
-  gpio.output(33,gpio.LOW)
-  gpio.output(31,gpio.HIGH)
-  time.sleep(1)
-  gpio.output(31,gpio.LOW)
+try:
+    for i in range(3):              # Ampel 3 mal durchschalten
+        gpio.output(29,gpio.HIGH)
+        time.sleep(3)
+        gpio.output(31,gpio.HIGH)
+        time.sleep(1)
+        gpio.output(29,gpio.LOW)
+        gpio.output(31,gpio.LOW)
+        gpio.output(33,gpio.HIGH)
+        time.sleep(3)
+        gpio.output(33,gpio.LOW)
+        gpio.output(31,gpio.HIGH)
+        time.sleep(1)
+        gpio.output(31,gpio.LOW)
 
-gpio.cleanup()
+except KeyboardInterrupt:
+    print "KeyboardInterrupt"
+
+except:
+    print "General failure"
+
+finally:
+    gpio.cleanup()
