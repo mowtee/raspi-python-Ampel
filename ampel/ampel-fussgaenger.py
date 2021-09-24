d81937e4656adcbaaa8481d297f1fb0672e00065
mowtee <themowtee@protonmail.com>
2021-09-24 10:57:39 +0200

    ampel-fg mit schalter

--- ampel/ampel-fussgaenger.py
+++ ampel/ampel-fussgaenger.py
@@ -14,7 +14,8 @@ gpio.setup(24,gpio.IN)          # Ampelschalter
 # gpio.setup(22,gpio.IN)          # Abbruchschalter
 
 
-gpio.output(33,gpio.HIGH)
+gpio.output(33,gpio.HIGH)       # Auto grün
+gpio.output(35,gpio.HIGH)       # FG rot
 time.sleep(3)
 
 # GPIO.add_event_detect(22, GPRIO.RISING)
@@ -28,17 +29,22 @@ try:
         time.sleep(2)
         gpio.output(31,gpio.LOW)
         gpio.output(29,gpio.HIGH)
+        gpio.output(35,gpio.LOW)
+        gpio.output(37,gpio.HIGH)
         time.sleep(5)
+        gpio.output(35,gpio.HIGH)
+        gpio.output(37,gpio.LOW)
         gpio.output(31,gpio.HIGH)
         time.sleep(1)
         gpio.output(29,gpio.LOW)
         gpio.output(31,gpio.LOW)
 
         gpio.output(33,gpio.HIGH)
-        GPIO.wait_for_edge(24, GPRIO.FALLING)
+        gpio.wait_for_edge(24, gpio.FALLING)
 
-        if GPIO.event_detected(22):
-            break
+
+#        if GPIO.event_detected(22):
+#            break
 
 
     gpio.cleanup()
