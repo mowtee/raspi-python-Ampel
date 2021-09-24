e225eed44abceae5fca285b51aef12f8329b5441
mowtee <themowtee@protonmail.com>
2021-09-24 10:47:02 +0200

    typo

--- ampel/schalter-test.py
+++ ampel/schalter-test.py
@@ -5,5 +5,5 @@ gpio.setmode(gpio.BOARD)        # definiere GPIO Overlay
 
 gpio.setup(24,gpio.IN)          # Schalter
 
-GPIO.wait_for_edge(24, GPRIO.FALLING)
+GPIO.wait_for_edge(24, GPIO.FALLING)
 print("Hallo")
