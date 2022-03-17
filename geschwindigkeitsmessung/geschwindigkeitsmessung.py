eeda8a9eb43147f71d490acee16ec70e8d28d93f
mowtee <themowtee@protonmail.com>
2022-03-17 23:07:51 +0100

    aufgeräumt

--- geschwindigkeitsmessung/geschwindigkeitsmessung.py
+++ geschwindigkeitsmessung/geschwindigkeitsmessung.py
@@ -1,6 +1,6 @@
 import RPi.GPIO as gpio         # importiere Bibliotheken
 import time
-from lcd import LCD
+from lcd import LCD             # LCD Bibliothek als Modul importieren und im globalen Namespace verfügbar machen
 
 gpio.setmode(gpio.BOARD)        # definiere GPIO Overlay
 gpio.setup(38,gpio.IN)          # definiere Pin 38 als Input
@@ -8,16 +8,16 @@ gpio.setup(40,gpio.IN)          # definiere Pin 40 als Input
 
 
 ns_pro_s = 1000000000
-distanz = 0.1                  # Distanz zwischen Lichtschranken in Metern
-geschwindigkeit_ms = 0
+distanz = 0.1                   # Distanz zwischen Lichtschranken in Metern
+geschwindigkeit_ms = 0          
 geschwindigkeit_kmh = 0
 zeit1 = None
 zeit2 = None
-geschwindigkeit_schwelle_kmh = 5
+geschwindigkeit_schwelle_kmh = 5    # erlaubte Geschwindigkeit
 anzeigedauer_sek = 3
 
 
-lcd = LCD()
+lcd = LCD()                     # LCD initialisieren
 
 
 
@@ -26,7 +26,7 @@ try:
         if time.time() > (messzeitpunkt+anzeigedauer_sek):
             lcd.clear()
         if gpio.input(38) == gpio.HIGH and gpio.input(40) == gpio.LOW and zeit1 == None:
-            zeit1 = time.time_ns()
+            zeit1 = time.time_ns()                      # time_ns() gibt die Zeit in ns seit der Unix Epoche zurück -> benötigt für Präzision
         if gpio.input(40) == gpio.HIGH and zeit1 and zeit2 == None:
             zeit2 = time.time_ns()
             zeit = zeit2 - zeit1
@@ -45,8 +45,6 @@ try:
                 lcd.message("Danke! :)\nSie fahren "+str(round(geschwindigkeit_kmh,2)+" km/h"))
 
 
-except KeyboardInterrupt:
+except KeyboardInterrupt:                       # Benötigt, um GPIO zu releasen und sauber zu beenden
     gpio.cleanup()
     exit()
-
-
